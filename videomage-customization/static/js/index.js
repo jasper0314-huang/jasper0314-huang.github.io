58f7756e27cb4b33710adbbe31795b157f2abbb3
jasper0314-huang <49524697+jasper0314-huang@users.noreply.github.com>
2025-03-28 06:15:38 +0000

    Deploying to gh-pages from @ jasper0314-huang/jasper0314-huang.github.io@c49b7abc3a8d53e940416ed1d472d6d823dee3c5 🚀

---
+++ videomage-customization/static/js/index.js
@@ -0,0 +1,1 @@
+window.HELP_IMPROVE_VIDEOJS=!1,$(document).ready((function(){var a={slidesToScroll:1,slidesToShow:1,loop:!0,infinite:!0,autoplay:!0,autoplaySpeed:5e3},l=(bulmaCarousel.attach(".carousel:not(#qualitative-carousel)",a),{slidesToScroll:1,slidesToShow:2,loop:!0,infinite:!0,autoplay:!0,autoplaySpeed:5e3});bulmaCarousel.attach("#qualitative-carousel",l);bulmaSlider.attach()}));
