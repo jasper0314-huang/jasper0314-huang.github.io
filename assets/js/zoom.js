954f3426dd07dec93a3a526355fc966f9a3b30c0
jasper0314-huang <49524697+jasper0314-huang@users.noreply.github.com>
2026-01-16 17:07:12 +0000

    Deploying to gh-pages from @ jasper0314-huang/jasper0314-huang.github.io@2f9519f6cb4fff94a1ea02ee2824e856f294f5fc 🚀

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee",margin:48})}));
+$(document).ready((function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee",margin:100})}));
